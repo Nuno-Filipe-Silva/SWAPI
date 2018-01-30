170176add0337f6172837d8ad57e290ab597278b
Vincent GANNEAU <vganneau@kayentis.com>
2018-01-30 21:59:07 +0100

    Improved planet data access object.

--- app/src/main/java/com/vincentganneau/swapi/model/dao/PlanetDao.java
+++ app/src/main/java/com/vincentganneau/swapi/model/dao/PlanetDao.java
@@ -12,28 +12,28 @@ import java.util.List;
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
 /**
- * The {@link Planet} data access object.
+ * The data access object for planets.
  * @author Vincent Ganneau
  */
 @Dao
 public interface PlanetDao {
 
     /**
-     * Inserts a {@link Planet} object in the database.
+     * Inserts a single planet in the database.
      * @param planet the {@link Planet} object to be inserted.
      */
     @Insert(onConflict = REPLACE)
     void insertPlanet(Planet planet);
 
     /**
-     * Inserts a {@link List} of {@link Planet} objects in the database.
+     * Inserts multiple planets in the database.
      * @param planets the {@link List} of {@link Planet} objects to be inserted.
      */
     @Insert(onConflict = REPLACE)
     void insertPlanets(List<Planet> planets);
 
     /**
-     * Loads all {@link Planet} objects from the database.
+     * Loads all the planets from the database.
      * @return the {@link List} of {@link Planet} objects as an observable {@link LiveData}
      */
     @Query("SELECT * FROM " + Planet.TABLE_NAME + " ORDER BY " + Planet.COLUMN_NAME)
